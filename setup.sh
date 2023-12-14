fd7b9e0ec4c483a27b8d23714925ae58412b95eb
mattclifford1 <mattc863@hotmail.com>
2023-12-14 13:34:51 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ echo 'alias Syu="sudo apt update && sudo apt upgrade -y"' >> ~/.bashrc
 source ~/.bashrc
 sudo apt update && sudo apt upgrade -y
 
+sudo apt remove pulseaudio -y
+# sudo apt install alsa-base
+
 # cd ~/Downloads
 # wget https://dtcooper.github.io/raspotify/raspotify-latest_arm64.deb
 # sudo apt install ./raspotify-latest_arm64.deb
