416d02627b12a19dab3ef58332db159806afd237
mattclifford1 <mattc863@hotmail.com>
2023-12-14 12:38:29 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo 'alias Syu="sudo apt update && sudo apt upgrade -y"' >> ~/.bashrc
 source ~/.bashrc
 Syu
 
-cd Downloads
+cd ~/Downloads
 wget https://dtcooper.github.io/raspotify/raspotify-latest_arm64.deb
 sudo apt install raspotify-latest_arm64.deb
-
+rm raspotify-latest_arm64.deb
