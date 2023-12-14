0e9447df314f034034a1977568a58dcf30c20379
mattclifford1 <mattc863@hotmail.com>
2023-12-14 12:42:49 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ echo 'alias Syu="sudo apt update && sudo apt upgrade -y"' >> ~/.bashrc
 source ~/.bashrc
 Syu
 
-cd ~/Downloads
-wget https://dtcooper.github.io/raspotify/raspotify-latest_arm64.deb
-sudo apt install raspotify-latest_arm64.deb
-rm raspotify-latest_arm64.deb
+# cd ~/Downloads
+# wget https://dtcooper.github.io/raspotify/raspotify-latest_arm64.deb
+# sudo apt install ./raspotify-latest_arm64.deb
+# rm raspotify-latest_arm64.deb
+
+sudo apt-get -y install curl && curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
