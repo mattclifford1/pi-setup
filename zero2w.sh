f62fd44211cb97940f795bc67834d5c066d64617
mattclifford1 <mattc863@hotmail.com>
2023-12-24 22:33:46 +0000

    Update zero2w.sh

--- zero2w.sh
+++ zero2w.sh
@@ -6,17 +6,23 @@ echo 'alias temp="watch -n 2 vcgencmd measure_temp"' >> ~/.bashrc
 sudo apt update && sudo apt upgrade -y
 sudo apt install git -y
 
+# modpiy
+git clone https://github.com/pimoroni/pirate-audio
+cd pirate-audio/mopidy
+sudo ./install.sh
+
 # remove light  -   https://forums.raspberrypi.com/viewtopic.php?t=328550
-echo 0  | sudo tee /sys/class/leds/led0/brightness
+# echo 0  | sudo tee /sys/class/leds/led0/brightness
 
 # add pirate audio DAC  -  https://github.com/pimoroni/pirate-audio
-echo "dtoverlay=hifiberry-dac" | sudo tee -a /boot/config.txt 
-echo "gpio=25=op,dh" | sudo tee -a /boot/config.txt 
+# echo "dtoverlay=hifiberry-dac" | sudo tee -a /boot/config.txt 
+# echo "gpio=25=op,dh" | sudo tee -a /boot/config.txt 
+
 # screen libs
-sudo apt install python-rpi.gpio python-spidev python-pip python-pil python-numpy
-sudo pip install st7789
+# sudo apt install python-rpi.gpio python-spidev python-pip python-pil python-numpy
+# sudo pip install st7789
 
-sudo apt-get -y install curl && curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 
-# you can edit the config in the file /etc/raspotify/conf
+# raspotify - you can edit the config in the file /etc/raspotify/conf
+# sudo apt-get -y install curl && curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 
