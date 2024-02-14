78303410189c311ee29be0f1bd1c1c278ac318d4
mattclifford1 <mattc863@hotmail.com>
2024-02-14 14:52:19 +0000

    Update pi4B_setup.sh

--- pi4B_setup.sh
+++ pi4B_setup.sh
@@ -8,11 +8,18 @@ echo "dtparam=pwr_led_activelow=off" | sudo tee -a /boot/config.txt
 echo "dtparam=act_led_trigger=none" | sudo tee -a /boot/config.txt
 echo "dtparam=act_led_activelow=off" | sudo tee -a /boot/config.txt
 
+# DAC
+echo "dtoverlay=iqaudio-dacplus" | sudo tee -a /boot/config.txt
+# comment out rasberry pi audo in config if needed
+
+
 source ~/.bashrc
 sudo apt update && sudo apt upgrade -y
 
 sudo apt remove pulseaudio -y
 # sudo apt install alsa-base
+
+sudo apt install sense-hat
 
 # cd ~/Downloads
 # wget https://dtcooper.github.io/raspotify/raspotify-latest_arm64.deb
